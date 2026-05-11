b35802b056a137a1ebabf0ef8d6a976fb3679e32
elm3rck <elm3rck@gmail.com>
2026-05-11 04:07:59 +0300

    fix steam provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,23 @@
 import NextAuth from "next-auth";
-import SteamProvider from "next-auth/providers/steam";
+import Steam from "next-auth-steam";
+import type { NextRequest } from "next/server";
 
-const handler = NextAuth({
-  providers: [
-    SteamProvider({
-      clientSecret: process.env.STEAM_API_KEY!,
-      callbackUrl:
-        "https://rust-site.pages.dev/api/auth/callback/steam",
-    }),
-  ],
+async function auth(
+  req: NextRequest,
+  ctx: {
+    params: {
+      nextauth: string[];
+    };
+  }
+) {
+  return NextAuth(req, ctx, {
+    providers: [
+      Steam(req, {
+        clientSecret: process.env.STEAM_SECRET!,
+      }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+}
 
-  secret: process.env.NEXTAUTH_SECRET,
-});
-
-export { handler as GET, handler as POST };
+export { auth as GET, auth as POST };
